a68768499d52390b6e0a7688cc838de3fd8ad66c
Bill Brousalis <bill.brousalis@gmail.com>
2022-08-02 22:56:45 +0100

    latest

--- server/server.py
+++ server/server.py
@@ -3,15 +3,14 @@ import socket
 
 class Server():
   def __init__(self):
-    self.test() 
+    self.host, self.port = socket.gethostname(), 9001
+    print(self.host, self.port)
+    #self.test() 
 
   def test(self):
-    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-    PORT = 65000  # Port to listen on (non-privileged ports are > 1023)
-
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      s.bind((HOST, PORT))
-      s.listen()
+      s.bind((self.host, self.port))
+      s.listen(1)
       conn, addr = s.accept()
       with conn:
         print(f"Connected by {addr}")
@@ -24,4 +23,3 @@ class Server():
 if __name__ == "__main__":
   #--testing
   s = Server()
-
