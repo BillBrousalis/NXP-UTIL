b955e49d090bc8622dbd87ad1df42647823f05c9
BillBrousalis <bill.brousalis@gmail.com>
2022-08-07 05:01:42 +0300

    threading working

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 #=============================================================================
-# Name        : CarVisGUI
+# Name        : CarDbgGUI
 # Author      : Basilis Mprousalis
 # Version     : 0.1
 # Year        : 2022
 # Description : Graphical User Interface For LineScan Camera Data Visualizatin
 #==============================================================================
 import os
+import time
+import random
 import threading
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.font as tkFont
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 def get_base_dir():
@@ -28,22 +31,26 @@ def get_config():
 def threadexec(func):
   def wrapper(*args, **kwargs):
     t = threading.Thread(target=func, args=args, kwargs=kwargs)
+    t.daemon = True
     t.start()
     return t
   return wrapper
 
 class Gui(tk.Tk):
-  _TITLE = 'LINESCAN VISUALIZATION'
+  _TITLE = 'LINESCAN DATA VISUALIZATION'
   _WIDTH = 700
   _HEIGHT = 500
   _COLORS = {'white': '#fafafa',
             'black': '#0f0f0f',
-            'grey': '#707070'
+            'grey': '#707070',
+            'light blue': '#d8e6f2'
   }
+  _ICON = os.path.join(get_base_dir(), 'assets/icon.ico')
   _CONFIG = get_config()
   def __init__(self):
     super().__init__()
     self.title(self._TITLE)
+    self.iconbitmap(self._ICON)
     self._FONT = tkFont.Font(family='Cascadia Code', size=13, weight='bold')
     self._FONTSMALL = tkFont.Font(family='Cascadia Code', size=10, weight='bold')
     self.draw_gui()
@@ -59,51 +66,87 @@ class Gui(tk.Tk):
     vsep.place(relx=0.68, rely=0.015, relwidth=0.002, relheight=0.97)
     # "Steering Angle" Label
     steerlb = tk.Label(self.canvas, text='Steering Angle:', font=self._FONT, bg=self._COLORS['white'])
-    steerlb.place(anchor='n', relx=0.8, rely=0.05, relwidth=0.225, relheight=0.05)
+    steerlb.place(anchor='n', relx=0.8, rely=0.09, relwidth=0.225, relheight=0.05)
     # Steering value displayed
-    steervallb = tk.Label(self.canvas, text='[ 0 ]', font=self._FONT, bg=self._COLORS['white'])
-    steervallb.place(anchor='n', relx=0.85, rely=0.1, relwidth=0.225, relheight=0.05)
+    self.steervallb = tk.Label(self.canvas, text='[ 0.0 ]', font=self._FONT, bg=self._COLORS['white'])
+    self.steervallb.place(anchor='n', relx=0.85, rely=0.14, relwidth=0.225, relheight=0.05)
     # Horizontal separator
     hsep = ttk.Separator(self.canvas, orient='horizontal')
-    hsep.place(relx=0.69, rely=0.178, relwidth=0.3, relheight=0.01)
+    hsep.place(relx=0.69, rely=0.218, relwidth=0.3, relheight=0.01)
     # "Speed" Label
     speedlb = tk.Label(self.canvas, text='Speed:', font=self._FONT, bg=self._COLORS['white'])
-    speedlb.place(anchor='n', relx=0.738, rely=0.2, relwidth=0.1, relheight=0.05)
+    speedlb.place(anchor='n', relx=0.738, rely=0.24, relwidth=0.1, relheight=0.05)
     # Speed value displayed
-    speedvallb = tk.Label(self.canvas, text='[ 0 ]', font=self._FONT, bg=self._COLORS['white'])
-    speedvallb.place(anchor='n', relx=0.85, rely=0.25, relwidth=0.225, relheight=0.05)
+    self.speedvallb = tk.Label(self.canvas, text='[ 0.0 ]', font=self._FONT, bg=self._COLORS['white'])
+    self.speedvallb.place(anchor='n', relx=0.85, rely=0.29, relwidth=0.225, relheight=0.05)
+    # Start-Stop button
+    self.but = tk.Button(self.canvas, text='START', font=self._FONT, bg=self._COLORS['white'], relief='groove', command=self.but_func)
+    self.but.bind('<Enter>', lambda event, x=self.but : self.but_hover(x))
+    self.but.bind('<Leave>', lambda event, x=self.but : self.but_hover_leave(x))
+    self.but.place(anchor='n', relx=0.85, rely=0.03, relwidth=0.2, relheight=0.05)
     # Version label
-    vers = tk.Label(self.canvas, text=f"[ v{self._CONFIG['VERSION']} ]", font=self._FONTSMALL, bg=self._COLORS['white'])
-    vers.place(anchor='n', relx=0.95, rely=0.955, relwidth=0.09, relheight=0.04)
+    vers = tk.Label(self.canvas, text=f"[ Bill Brousalis  v{self._CONFIG['VERSION']} ]", font=self._FONTSMALL, bg=self._COLORS['white'])
+    #vers.place(anchor='n', relx=0.95, rely=0.955, relwidth=0.09, relheight=0.04)
+    vers.place(anchor='n', relx=0.85, rely=0.955, relwidth=0.27, relheight=0.04)
 
   def draw_graph(self):
     self.fig = plt.Figure()
     self.fig.set_facecolor(self._COLORS['white'])
     self.graphcanvas = FigureCanvasTkAgg(self.fig, master=self.canvas)
-    self.graphcanvas.get_tk_widget().place(anchor='n', relx=0.36, rely=0.01, relwidth=0.7, relheight=0.98)
+    self.graphcanvas.get_tk_widget().place(anchor='n', relx=0.34, rely=0.01, relwidth=0.67, relheight=0.98)
     self.ax = self.fig.add_subplot(111)
-    self.ax.grid()
-    self.ax.set_title('LineScan Readings')
+    self.ax.axis((0, 127, 0, 2))
+    self.ax.set_title('--LineScan Readings--')
   
   def setup(self):
     self.DATA = {'LINE': [],
                  'STEER': 0,
                  'SPEED': 0
     }
+    self.isrunning = False
 
   @threadexec
   def tUpdatelbs(self):
-    import time
-    time.sleep(10)
-    pass
-    
+    while 1:
+      if not self.isrunning: return
+      self.steervallb['text'] = f'[ {str(random.randint(0, 10))}.0 ]'
+      print("[ Thread ] tUpdatelbs()")
+      time.sleep(0.5)
+
   @threadexec
   def tReaddat(self):
-    pass
+    while 1:
+      if not self.isrunning: return
+      print("[ Thread ] tReaddat()")
+      time.sleep(1)
 
   @threadexec
   def tAnim(self):
-    pass
+    self.anim = FuncAnimation(self.fig, self.update_graph, interval=250)
+    self.anim._start()
+  
+  def update_graph(self, i):
+    print("[ Thread ] ANIM()")
+    if not self.isrunning: self.anim.event_source.stop()
+    self.ax.clear()
+    datx, daty = [0, 50, 100], [random.randint(0, 2) for _ in range(3)]
+    self.ax.plot(datx, daty)
+
+  # Start-Stop button functionality
+  def but_func(self):
+    if self.isrunning:
+      self.isrunning = False
+      self.but['text'] = 'START'
+    else:
+      self.isrunning = True
+      self.tUpdatelbs()
+      self.tReaddat()
+      self.tAnim()
+      self.but['text'] = 'STOP'
+
+  # Visual
+  def but_hover(self, button, color=_COLORS['light blue']): button.configure(bg=color)
+  def but_hover_leave(self, button, color=_COLORS['white']): button.configure(bg=color)
 
   def exit(self):
     print("[-] Exiting...")
