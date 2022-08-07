eea0286262c4338e1e6feee9dc321d2c24ed14bb
Bill Brousalis <bill.brousalis@gmail.com>
2022-08-07 23:47:33 +0100

    new

--- src/raspberry-pi/run.py
+++ src/raspberry-pi/run.py
@@ -15,29 +15,46 @@ def get_config():
   with open(os.path.join(get_base_dir(), "config/config.yaml"), "r") as f:
     return yaml.safe_load(f)
 
+def argparser():
+  import sys
+  if len(sys.argv) < 2: return None
+  if sys.argv[1] == '-h': 
+    print('-h       Help'
+          '--dev    specify expected number of clients'
+          '   ex.   ./run.py --dev=3')
+    exit()
+  elif '--dev=' in sys.argv[1]: return int(sys.argv[1].split('=')[1])
+  else: raise Exception(f'Unknown argument: {sys.argv[1]}')
+
 def main():
   config = get_config()
-  s = server.Server(port=config["RPI-PORT"], max_dev=config["MAX-DEV"])
+  max_dev = argparser()
+  if max_dev is None: max_dev = config['MAX-DEV']
+  assert(max_dev > 0 and max_dev < 10)
+  print(f'[*] Expecting [ {max_dev} ] Client(s)')
+  s = server.Server(port=config["RPI-PORT"], max_dev=max_dev)
   u = uart.Uart(baud=config['UART-BAUD'])
   buf = None
   try:
     while 1: # mainloop
       buf = u.recvline().strip()
-      print(f'run.py got : {buf}')
+      print(f'[ DEBUG ] UART Received:\n{buf}')
+      # Pass buf to clients
       s.send(buf)
-
-      if config["COMMANDS"]:
+      '''
+      if config['COMMANDS']:
         #TODO: deal with commands
         pass
-      if config["LOGGING"] == "RPI":
+      if config['LOGGING'] == 'RPI':
         #TODO: deal with logging on rpi
         pass
+      '''
   except KeyboardInterrupt:
-    print("\n[-] Closing server & exiting...")
+    print('\n[-] Closing server & exiting...')
     s.close()
     exit()
   except Exception as e:
-    print(f"[-] Error: {e}")
+    print(f'[-] Error: {e}')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
