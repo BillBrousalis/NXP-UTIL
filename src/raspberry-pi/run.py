05343a48bdce24761e6ad62dda9a8c56653946ea
Bill Brousalis <bill.brousalis@gmail.com>
2022-08-13 23:43:57 +0100

    commands working

--- src/raspberry-pi/run.py
+++ src/raspberry-pi/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
-##### MODULES #####
+import math
+# MODULES
 import uart
 import server
 
@@ -28,22 +29,36 @@ def argparser():
   else: raise Exception(f'Unknown argument: {sys.argv[1]}')
 
 def main():
+  testspeed, teststeer = 0, 0
   config = get_config()
   max_dev = argparser()
   if max_dev is None: max_dev = config['MAX-DEV']
-  assert(max_dev > 0 and max_dev < 10)
+  assert(max_dev >= 0 and max_dev < 10)
   print(f'[*] Expecting [ {max_dev} ] Client(s)')
   s = server.Server(port=config["RPI-PORT"], max_dev=max_dev)
   u = uart.Uart(baud=config['UART-BAUD'])
   buf = None
   try:
     while 1: # mainloop
-      buf = u.recv(config['BYTES-PER-LINE'])
-      if config['DEBUG']: print(f'[ DEBUG ] UART Received:\n{buf}')
-      # Pass buf to client(s)
-      s.send(buf)
-      if config['COMMANDS']:
-        u.send(b'\x20\x30')
+      if max_dev != 0:
+        buf = u.recv(config['BYTES-PER-LINE'])
+        if config['DEBUG']: print(f'[ DEBUG ] UART Received:\n{buf}')
+        # Pass buf to client(s)
+        s.send(buf)
+      else: # COMMANDS unavailable while also having clients present TODO: have separate uart conn for data sending
+        if config['COMMANDS']:
+          # Test
+          testspeed += 0.0005
+          teststeer += 0.005
+          if testspeed > 30: testspeed = 25
+          if teststeer > 60: teststeer = -60
+          print(teststeer)
+          x = testspeed if testspeed > 0 else 255+testspeed
+          x = math.floor(x).to_bytes(1, 'little')
+          y = teststeer if teststeer > 0 else 255+teststeer
+          y = math.floor(y).to_bytes(1, 'little')
+          u.send(x)
+          u.send(y)
       '''
       if config['LOGGING'] == 'RPI':
         #TODO: deal with logging on rpi
